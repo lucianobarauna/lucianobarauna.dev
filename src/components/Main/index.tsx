daee83d8f1d3f4028174d676e5a033983c877923
Luciano Baraúna <baraunaluciano@gmail.com>
2022-03-11 22:47:35 -0300

    feat: change texts

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,20 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+  title = 'Olá, Me chamo Luciano Baraúna.',
+  description = 'Sou desenvolvedor Front-end. Apaixonado por css e acessibilidade'
 }) => (
   <S.Wrapper>
-    <S.Logo
+    {/* <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
+    /> */}
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
-    <S.Illustration
+    {/* <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
-    />
+    /> */}
   </S.Wrapper>
 )
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,7 +7,7 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /Olá, Me chamo Luciano Baraúna/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
